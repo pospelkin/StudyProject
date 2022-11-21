37a1a8a0e359df3e072c75c7d20adc258eb922c3
NIKOLINA\38095 <feschenkonikolina@gmail.com>
2022-11-21 10:57:52 +0300

    init commit

---
+++ Assets/scripts/LearningCurve.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    public int 
+    {
+        Debug.LogFormat(int sum);
+        return sum;
+    }
+    
+}
